4547fa5879d121ec4efc31e60534c1f9a2baeaa7
k98a73 <100816862+k98a73@users.noreply.github.com>
2022-08-15 11:41:06 +0900

    #51 add: Top画面のSTATUSのSelectによるフィルター機能追加

--- util/filterTodoList.ts
+++ util/filterTodoList.ts
@@ -4,9 +4,19 @@ export const filterTodoList = (
   prioritySelect: string,
   todoList: any
 ) => {
+  // STATUS SELECTが何も選択されていなければ、3
+  let status = 3;
+  if (statusSelect === "") {
+    status = 3;
+  } else {
+    status = Number(statusSelect);
+  }
   let filterTodos: string[] = [];
   todoList.filter((todo: any) => {
-    if (word !== "" && todo.title.match(word)) {
+    if (
+      (word === "" || todo.title.match(word)) &&
+      (status === 3 || todo.status === status)
+    ) {
       filterTodos.push(todo);
     }
   });
