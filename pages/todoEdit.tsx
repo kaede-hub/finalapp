ab6f92c201e3f21dfbcf1de0c32c1074d4cb8807
hajimev7 <48694062v8@gmail.com>
2022-08-21 10:21:51 +0900

    use react form/update機能の追加

--- pages/todoEdit.tsx
+++ pages/todoEdit.tsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import Head from "next/head";
 import {
-  Box,
   Button,
   Container,
   Flex,
   FormControl,
+  FormErrorMessage,
   FormLabel,
   Input,
   Spacer,
@@ -13,10 +13,82 @@ import {
   Textarea,
   VStack,
 } from "@chakra-ui/react";
-
+import { SubmitHandler, useForm } from "react-hook-form";
+import { useRecoilState } from "recoil";
+import { useRouter } from "next/router";
 import { Header } from "../components/Header";
+import { todoListState } from "../constants/atom";
 
-export default function TodoEdit() {
+type FormInput = {
+  title: string;
+  detail: string;
+  priority: string;
+};
+
+type todoList = {
+  id: null | number;
+  title: null | string;
+  detail: null | string;
+  // 0:NOT STARTED、1:DOING、2:DONE
+  status: null | 0 | 1 | 2;
+  priority: null | string;
+  createAt: null | Date;
+  EditAt: null | Date;
+  // all:TOPページ等に表示されるTODO LIST、draft:DRAFTページ、trash:trashページ
+  category: "all" | "draft" | "trash";
+};
+
+type category = "all" | "draft" | "trash";
+
+export default function Edit() {
+  const [value, setValue] = useState("High");
+  const [category, setCategory] = useState<category>("all");
+  const [todoList, setTodoList] = useRecoilState<any>(todoListState);
+  const date = new Date()
+  const year = date.getFullYear()
+  const month = date.getMonth() + 1
+  const day = date.getDate()
+  const hours = date.getHours();
+  const minutes = date.getMinutes()
+  
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm<FormInput>();
+  const router = useRouter();
+
+  // idを取得する関数
+  const getId = () => {
+    if (todoList.length === 0) {
+      // todoListが空なら、1を返す
+      return 1;
+    } else {
+      // todoListが空でないなら、配列の最後に入っているidに+1した値を返す
+      return todoList[todoList.length - 1].id + 1;
+    }
+  };
+
+  const onSubmit: SubmitHandler<FormInput> = ({ title, detail, priority }) => {
+    setTodoList((oldTodoList: Array<todoList>) => [
+      ...oldTodoList,
+      {
+        id: getId(),
+        title,
+        detail,
+        status: 0,
+        priority,
+        EditAt: new Date(),
+        category,
+      },
+    ]);
+    if (category === "draft") {
+      router.push("/draft");
+    } else {
+      router.push("/Top");
+    }
+  };
+
   return (
     <>
       <Head>
@@ -46,55 +118,72 @@ export default function TodoEdit() {
               borderWidth="1px"
               borderColor="blackAlpha.800"
               borderRadius="50px"
+              onClick={() => router.push("/Top")}
             >
               Back
             </Button>
           </Flex>
-          <FormControl>
-            <FormLabel
-              m="0"
-              fontSize="24px"
-              fontWeight="bold"
-              lineHeight="24px"
-              color="blackAlpha.800"
-            >
-              TITLE
-            </FormLabel>
-            <Input
-              h="71px"
-              mt="5px"
-              p="8px 16px"
-              fontSize="20px"
-              fontWeight="bold"
-              color="blackAlpha.800"
-              borderWidth="1px"
-              borderColor="blackAlpha.800"
-              borderRadius="10px"
-            />
-          </FormControl>
-          <FormControl>
-            <FormLabel
-              // FormControlにmt:8pxがあるため、FormLabelのmtは8pxに設定
-              m="8px 0 0 0"
-              fontSize="24px"
-              fontWeight="bold"
-              lineHeight="24px"
-              color="blackAlpha.800"
-            >
-              DETAIL
-            </FormLabel>
-            <Textarea
-              h="287px"
-              mt="4px"
-              fontSize="20px"
-              fontWeight="bold"
-              color="blackAlpha.800"
-              borderWidth="1px"
-              borderColor="blackAlpha.800"
-              borderRadius="10px"
-            />
-          </FormControl>
-          <Flex w="100%">
+          <form style={{ width: "100%" }} onSubmit={handleSubmit(onSubmit)}>
+            <FormControl isInvalid={errors.title ? true : false}>
+              <FormLabel
+                m="0"
+                fontSize="24px"
+                fontWeight="bold"
+                lineHeight="24px"
+                color="blackAlpha.800"
+                htmlFor="title"
+              >
+                TITLE
+              </FormLabel>
+              <Input
+                id="title"
+                h="72px"
+                mt="4px"
+                p="8px 16px"
+                fontSize="24px"
+                fontWeight="bold"
+                color="blackAlpha.800"
+                borderWidth="1px"
+                borderColor="blackAlpha.800"
+                borderRadius="10px"
+                {...register("title", {
+                  required: "TITLEは必須です",
+                })}
+              />
+              <FormErrorMessage>
+                {errors.title && errors.title.message}
+              </FormErrorMessage>
+            </FormControl>
+            <FormControl isInvalid={errors.detail ? true : false}>
+              <FormLabel
+                // FormControlにmt:8pxがあるため、FormLabelのmtは16pxに設定
+                m="16px 0 0 0"
+                fontSize="24px"
+                fontWeight="bold"
+                lineHeight="24px"
+                color="blackAlpha.800"
+              >
+                DETAIL
+              </FormLabel>
+              <Textarea
+                id="detail"
+                h="208px"
+                mt="4px"
+                fontSize="24px"
+                fontWeight="bold"
+                color="blackAlpha.800"
+                borderWidth="1px"
+                borderColor="blackAlpha.800"
+                borderRadius="10px"
+                {...register("detail", {
+                  required: "DETAILは必須です",
+                })}
+              />
+              <FormErrorMessage>
+                {errors.detail && errors.detail.message}
+              </FormErrorMessage>
+            </FormControl>
+
             <Flex
               // Flexにmt:8pxがあるため、Buttonのmtは16pxに設定
               mt="16px"
@@ -118,6 +207,7 @@ export default function TodoEdit() {
                   2020-11-8 18:55
                 </Text>
               </Flex>
+
               <Flex ml="27px" direction="column">
                 <Text
                   fontSize="16px"
@@ -134,28 +224,32 @@ export default function TodoEdit() {
                   lineHeight="20px"
                   color="blackAlpha.800"
                 >
-                  2020-11-8 18:55
+                  <p>{`${year}-${month}-${day} ${hours}:${minutes}`}</p>
                 </Text>
               </Flex>
             </Flex>
-            <Spacer />
-            <Button
-              w="112px"
-              h="40px"
-              // Flexにmt:8pxがあるため、Buttonのmtは16pxに設定
-              mt="16px"
-              p="0"
-              fontSize="18px"
-              fontWeight="bold"
-              bg="green.600"
-              color="green.50"
-              borderWidth="1px"
-              borderColor="blackAlpha.800"
-              borderRadius="50px"
-            >
-              UPDATE
-            </Button>
-          </Flex>
+            
+            <Flex w="100%" flexDirection="row-reverse">
+              <Button
+                type="submit"
+                w="112px"
+                h="40px"
+                // Flexにmt:8pxがあるため、Buttonのmtは4pxに設定
+                m="4px 0 0 8px"
+                p="0"
+                fontSize="18px"
+                fontWeight="bold"
+                bg="green.600"
+                color="green.50"
+                borderWidth="1px"
+                borderColor="blackAlpha.800"
+                borderRadius="50px"
+                onClick={() => setCategory("all")}
+              >
+                UPDATE
+              </Button>
+            </Flex>
+          </form>
         </VStack>
       </Container>
     </>
