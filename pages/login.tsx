7506b821798a1ffedeeb48a07507301562d5cdaf
hajimev7 <48694062v8@gmail.com>
2022-09-07 21:55:54 +0900

    loginの文字の色を変更

--- pages/login.tsx
+++ pages/login.tsx
@@ -21,7 +21,7 @@ const login = () =>{
             <Box mt="5" ml="5px" bg="white" h='30px' w="100px" borderRadius="3xl" textAlign='center' color="green.500"> EMAIL</Box>
             <Input bg="white" mt="25" ml="50" mr="5px"  w="80%"  placeholder='Email'borderRadius="3xl"/>
             <Center>
-              <Button w="25%" mt="5"　bg="green.400" borderRadius="3xl">Login</Button>              
+              <Button w="25%" mt="5"　bg="green.400" color="white" borderRadius="3xl">Login</Button>              
             </Center>
             <Center>
               <Button w="50%" mt="5" bg="gray.100" _hover={{ bg: "red" }} color="white" borderRadius="3xl">GOOGLELOGIN</Button>
